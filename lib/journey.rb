cf9f49a0bed5110ab3e9c9ea91caea1bc2061a3e
unicornelia <ez.kornelia@gmail.com>
2016-11-18 12:34:55 +0000

    Task 13 half way through, added journey start and journey end attr

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,10 +2,29 @@
 
 #start journey
 #end journey
-#register entry station
 #journey complete?
 
 
 #call fare
 #entry station
 #exit station
+
+class Journey
+
+  attr_accessor :journey_started, :journey_finished
+
+  def initialize
+    @journey_started = false
+    @journey_finished = false
+  end
+
+def begin_journey
+  self.journey_started = true
+end
+
+def end_journey
+  self.journey_finished = true
+end
+
+
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,32 @@
+require 'journey'
+
+describe Journey do
+
+  let(:journey) { described_class.new }
+
+  it 'expects the journey class to exist' do
+    expect(journey).to be_an_instance_of(Journey)
+  end
+
+  context 'beginning and ending journeys' do
+
+    it 'expects begin_journey to' do
+      journey.begin_journey
+      expect(journey.journey_started).to eq true
+    end
+
+    it 'expects end_journey to' do
+      journey.end_journey
+      expect(journey.journey_finished).to eq true
+    end
+
+    it 'expects the Journey to have journey started (set to false) attributes' do
+      expect(journey.journey_started).to eq false
+    end
+
+    it 'expects the Journey to have journey finished (set to false) attributes' do
+      expect(journey.journey_finished).to eq false
+    end
+
+  end
+end
